0439ca768b97f96ede330c7a4fbf773bcff83a57
Duongnv2 <duongnv2@nexttech.asia>
2019-01-13 09:30:56 +0700

    update rational

--- Rationals/Task/src/rationals/Rational.kt
+++ Rationals/Task/src/rationals/Rational.kt
@@ -1,8 +1,54 @@
 package rationals
 
+import java.math.BigInteger
+
+fun gcd(a: BigInteger, b: BigInteger): BigInteger = if (b == BigInteger.ZERO) a else gcd(b, a.mod(b))
+fun lcm(a: BigInteger, b: BigInteger) = (a.times(b)).div(gcd(a, b))
+
+class Rational(val numerator: BigInteger, val denominator: BigInteger) {
+    init {
+        if (BigInteger.ZERO == denominator) {
+            throw IllegalArgumentException("denominator must be not 0")
+        }
+    }
+
+    operator fun plus(other: Rational): Rational {
+        return if (denominator == other.denominator) {
+            Rational(numerator + other.numerator, denominator)
+        } else {
+            val lcm = lcm(denominator, other.denominator)
+            Rational(this.rebaseNumerator(lcm) + other.rebaseNumerator(lcm), lcm)
+        }
+    }
+
+    private fun rebaseNumerator(lcm: BigInteger) = numerator * (lcm / denominator)
+
+}
 
 
-fun main() {
+fun String.toRational(): Rational {
+
+    if (Regex("""^\d+/\d+$""").matches(this)) {
+        throw IllegalArgumentException("String must be formatted: 'numerator/denominator'");
+    }
+
+    val split = this.split('/')
+    return Rational(split[0].toBigInteger(), split[1].toBigInteger())
+}
+
+infix fun BigInteger.divBy(denominator: BigInteger): Rational {
+    return Rational(this, denominator)
+}
+
+infix fun Long.divBy(denominator: Long): Rational {
+    return Rational(this.toBigInteger(), denominator.toBigInteger())
+}
+
+infix fun Int.divBy(denominator: Int): Rational {
+    return Rational(this.toBigInteger(), denominator.toBigInteger())
+}
+
+fun main(args: Array<String>) {
     val half = 1 divBy 2
     val third = 1 divBy 3
 
@@ -35,3 +81,5 @@ fun main() {
     println("912016490186296920119201192141970416029".toBigInteger() divBy
             "1824032980372593840238402384283940832058".toBigInteger() == 1 divBy 2)
 }
+
+
